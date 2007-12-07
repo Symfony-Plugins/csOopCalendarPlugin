9561b7509014d889bf04cac423220c995c0eef1b
Josh.Reynolds <Josh.Reynolds@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-12-06 22:37:51 -0600

    initial import of generated plugin with base classes (that don't do anything yet)
    git-svn-id: http://svn.symfony-project.com/plugins/csOopCalendarPlugin/trunk@6346 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/BasecsCalendar.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the csCalendar package.
+ * (c) 2006-2007 Jonathan H. Wage <jonwage@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
+/**
+ * Base Symfony Calendar Class
+ *
+ * @package   csCalendar
+ * @author    Josh R Reynolds <reynoldsj@gmail.com>
+ * @version   SVN: $Id:
+ **/
+class BasecsCalendar
+{
+  
+}
+?>
---
+++ lib/csCalendar.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the csCalendar package.
+ * (c) 2007-2008 Josh R. Reynolds <reynoldsj@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
+/**
+ *Symfony Calendar Class
+ *
+ * @package   csCalendar
+ * @author    Josh R. Reynolds <reynoldsj@gmail.com>
+ * @version   SVN: $Id:
+ **/
+class csCalendar extends BasecsCalendar
+{
+
+}
