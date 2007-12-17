60867cca0579c21ea4bc58ee2d8ca4b0b34a5988
Josh.Reynolds <Josh.Reynolds@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-12-17 10:33:21 -0600

    update objects, still not in a usable form
    git-svn-id: http://svn.symfony-project.com/plugins/csOopCalendarPlugin/trunk@6539 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/csCalendarHelper.php
+++ lib/helper/csCalendarHelper.php
@@ -1,3 +1,16 @@
 <?php 
-
+function cs_calendar_display($calendar = )
+{
+  $out = "<!--  START CALENDAR DISPLAY-->\n\n";
+  $out .= "<div class=\"calendar-wrapper\"\n";
+  $out .= "  <div class=\"".$calendar->getClass()."\" id=\"calendar-".$calendar->getIdHash()."\">\n";
+  foreach($calendar->getDays() as $day):
+    $out .= "    <div class=\"".$day->getClass()."\" id=\"day-".$day->getIDHash()."\">\n";
+    
+    $out .= "    </div> <!-- END DAY -->\n";
+  endforeach;
+  $out .= "  </div> <!-- END CALENDAR -->\n";
+  $out .= "</div> <!-- END CALENDAR WRAPPER -->\n\n";
+  $out .= "<!-- END CALENDAR DISPLAY-->";
+}
 ?>
