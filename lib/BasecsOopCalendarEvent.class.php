0633ae2c70b34184489e7286df77671d4e36cc04
Josh.Reynolds <Josh.Reynolds@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-02-24 14:40:57 -0600

    fixed bugs in attribute parsing from arrays
    git-svn-id: http://svn.symfony-project.com/plugins/csOopCalendarPlugin/trunk@7597 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/BasecsOopCalendarEvent.class.php
+++ lib/BasecsOopCalendarEvent.class.php
@@ -19,77 +19,85 @@ class BasecsOopCalendarEvent
 {
   //class attributes based on the RFC 2445 iCalendar Spec
   //found at http://tools.ietf.org/html/rfc2445
-  protected $_init_date     = date('U'),
-            $_attributes    = array(
-              'class'       => 'event', //event class
-              'created'     => $this->_init_date, //created at date
-              'description' => '', //text description
-              'dtstart'     => $this->_init_date, //start timestamp
-              'geo'         => '', //not sure what this is...
-              'last_mod'    => $this->_init_date, //last modification
-              'location'    => '', //location string
-              'organizer'   => '', //organizer
-              'summary'     => 'event', //summary
-              'transp'      => '', //also not sure...
-              'uid'         => '', 
-              'url'         => '', //where to get details
-              'recurid'     => '', //what other ones is this related to
-              'dtend'       => $this->_init_date, //end timestamp
-              'duration'    => 0, //duration in seconds
-              'attach'      => array(),
-              'attendee'    => array(),
-              'categories'  => array(),
-              'comment'     => array(),
-              'contact'     => array(),
-              'exdate'      => array(),
-              'exrule'      => array(),
-              'rstatus'     => array(),
-              'related'     => array(),
-              'resources'   => array(),
-              'rdate'       => array(),
-              'rrule'       => array(),
-              'xprop'       => array(),
-              ),
-            $_human_names   = array(
-              'class'       => 'Class', //event class
-              'created'     => 'Created At', //created at date
-              'description' => 'Description', //text description
-              'dtstart'     => 'Event Start', //start timestamp
-              'geo'         => 'Geographic Location', //not sure what this is...
-              'last_mod'    => 'Last Modified', //last modification
-              'location'    => 'Location', //location string
-              'organizer'   => 'Organizer', //organizer
-              'summary'     => 'Summary', //summary
-              'transp'      => 'Transp', //also not sure...
-              'uid'         => 'UID', 
-              'url'         => 'URL', //where to get details
-              'recurid'     => 'Recurrence ID', //what other ones is this related to
-              'dtend'       => 'Event End', //end timestamp
-              'duration'    => 'Duration', //duration in seconds
-              'attach'      => 'Attachments',
-              'attendee'    => 'Attendee',
-              'categories'  => 'Category',
-              'comment'     => 'Comment',
-              'contact'     => 'Contact',
-              'exdate'      => 'ExDate',
-              'exrule'      => 'ExRule',
-              'rstatus'     => 'RStatus',
-              'related'     => 'Related',
-              'resources'   => 'Resources',
-              'rdate'       => 'RDate',
-              'rrule'       => 'RRule',
-              'xprop'       => 'XProp',
-              ),
-            $_date_format   = 'U',
-            $_dates         = array(
-              'created',
-              'dtstart',
-              'last_mod',
-              'dtend').
-            $_minute_percent  = 100/(60*24); //100%/(minutes*hours)
-              
    public function __construct()
    {
+     //for logging to symfony
+      if (sfConfig::get('sf_logging_enabled'))
+      {
+        $this->_logger = sfContext::getInstance()->getLogger();
+      }
+      
+     //build all the fixtures we need
+     $this->_init_date     = date('U');
+     $this->_attributes    = array(
+       'CLASS'       => 'event', //event class
+       'CREATED'     => $this->_init_date, //created at date
+       'DESCRIPTION' => '', //text description
+       'DTSTART'     => $this->_init_date, //start timestamp
+       'GEO'         => '', //not sure what this is...
+       'LAST_MOD'    => $this->_init_date, //last modification
+       'LOCATION'    => '', //location string
+       'ORGANIZER'   => '', //organizer
+       'SUMMARY'     => 'event', //summary
+       'TRANSP'      => '', //also not sure...
+       'UID'         => '', 
+       'URL'         => '', //where to get details
+       'RECURID'     => '', //what other ones is this related to
+       'DTEND'       => $this->_init_date, //end timestamp
+       'DURATION'    => 0, //duration in seconds
+       'ATTACH'      => array(),
+       'ATTENDEE'    => array(),
+       'CATEGORIES'  => array(),
+       'COMMENT'     => array(),
+       'CONTACT'     => array(),
+       'EXDATE'      => array(),
+       'EXRULE'      => array(),
+       'RSTATUS'     => array(),
+       'RELATED'     => array(),
+       'RESOURCES'   => array(),
+       'RDATE'       => array(),
+       'RRULE'       => array(),
+       'XPROP'       => array(),
+       );
+     $this->_human_names   = array(
+       'CLASS'       => 'Class', //event class
+       'CREATED'     => 'Created At', //created at date
+       'DESCRIPTION' => 'Description', //text description
+       'DTSTART'     => 'Event Start', //start timestamp
+       'GEO'         => 'Geographic Location', //not sure what this is...
+       'LAST_MOD'    => 'Last Modified', //last modification
+       'LOCATION'    => 'Location', //location string
+       'ORGANIZER'   => 'Organizer', //organizer
+       'SUMMARY'     => 'Summary', //summary
+       'TRANSP'      => 'Transp', //also not sure...
+       'UID'         => 'UID', 
+       'URL'         => 'URL', //where to get details
+       'RECURID'     => 'Recurrence ID', //what other ones is this related to
+       'DTEND'       => 'Event End', //end timestamp
+       'DURATION'    => 'Duration', //duration in seconds
+       'ATTACH'      => 'Attachments',
+       'ATTENDEE'    => 'Attendee',
+       'CATEGORIES'  => 'Category',
+       'COMMENT'     => 'Comment',
+       'CONTACT'     => 'Contact',
+       'EXDATE'      => 'ExDate',
+       'EXRULE'      => 'ExRule',
+       'RSTATUS'     => 'RStatus',
+       'RELATED'     => 'Related',
+       'RESOURCES'   => 'Resources',
+       'RDATE'       => 'RDate',
+       'RRULE'       => 'RRule',
+       'XPROP'       => 'XProp',
+       );
+     $this->_date_format   = 'U';
+     $this->_dates         = array(
+       'CREATED',
+       'DTSTART',
+       'LAST_MOD',
+       'DTEND');
+     $this->_minute_percent  = 100/(60*24); //100%/(minutes*hours);
+     $this->_timezone = '';
+     
      $args = func_get_args();
      
      //if we got any arguments, parse them
@@ -110,10 +118,20 @@ class BasecsOopCalendarEvent
        }
        elseif(is_array($args[0]))
        {
-         $this->parseFromArray();
+         $this->parseFromArray($args[0]);
        }
      }
-     
+   }
+   
+   public function __toString($format = "icalendar")
+   {
+     switch($format)
+     {
+        case 'icalendar':
+          $out = $this->_buildICalendar();
+          break;
+     }
+     return $out;
    }
    
    public function getIdHash()
@@ -157,40 +175,15 @@ class BasecsOopCalendarEvent
    {
      foreach($array as $attribute => $value)
      {
-       $this->parseAttribute($attribute, $value);
+       $this->_logger->info('csOopCalendarEvent: Parsing Attribute = '.$attribute.' = '.$value);
+       $this->_parseAttribute($attribute, $value);
      }
-   }
-   
-   public function parseFromiCalendar($i_calendar='')
-   {
-    //do the parsing from the icalendar string here
-    
-    //not yet implemented
-   }
-   
-   protected function parseAttribute($attribute = '', $value='')
-   {
-    //find out if this is a valid attribute 
-    if(isset($this->_attributes[$attribute]))
-    {
-      //figture out what type of attribute it is
-      if(is_string($this->_attributes[$attribute]) && is_string($value))
-      {
-        $this->setAttribute($attribute, $value);
-      }
-      elseif(is_array($this->_attributes[$attribute]) && is_array($value))
-      {
-        foreach($value as $v)
-        {
-          $this->addAttribute($attribute, $value);
-        }
-      }
-    }
    }
    
    public function setAttribute($attribute='', $value)
    {
-     if($isset($this->_attributes[$attribute]) && $this->_sameType($this->_attributes[$attribute], $value))
+     if(isset($this->_attributes[$attribute]) &&
+       $this->_sameType($this->_attributes[$attribute], $value))
      {
        $this->_attributes[$attribute] = $value;
      }
@@ -202,6 +195,7 @@ class BasecsOopCalendarEvent
        && is_array($this->_attributes[$attribute])
        && is_string($value))
      {
+       $this->_logger->info('csOopCalendarEvent: Adding attribute - '.$attribute.' = '.$value);
        $this->_attributes[$attribute][] = $value;
      }
    }
@@ -243,6 +237,88 @@ class BasecsOopCalendarEvent
      return $this->_attributes;
    }
    
+   /**
+    * _parseAttribute
+    * Takes an attribute and value and parses into internal arrays.
+    * If the attribute has a timezone on it (ie DTSTART;TZID=US/CENTRAL)
+    * then we set the timezone for using in output later.
+    * Currently assumes that timezones for all dates in event are same.
+    *
+    * @param string $attribute The name of the attribute we are setting
+    * @param string $value 
+    * @return void
+    * @author Josh Reynolds
+    */
+   
+   protected function _parseAttribute($attribute = '', $value='')
+    {
+     //timezones with dates are formatted like DTSTART;TZID=US/CENTRAL
+     $attribute_array = explode(';', $attribute);
+     $this->_logger->info('csOopCalendarEvent: Trying to add attribute - '.$attribute_array[0].' = '.$value);
+     
+     //Check for attribute existence against local array
+     if(isset($this->_attributes[$attribute_array[0]]))
+     {
+       //If the full attribute name passed in matches (shortcut for non-dates)
+       if(isset($this->_attributes[$attribute]))
+       {
+         if(is_string($this->_attributes[$attribute]) && is_string($value))
+         {
+           $this->setAttribute($attribute, $value);
+         }
+         elseif(is_array($this->_attributes[$attribute]) && is_array($value))
+         {
+           foreach($value as $v)
+           {
+             $this->addAttribute($attribute, $value);
+           }
+         }
+       }
+       //if there is something in the second position of this array, 
+       // it was a date with a timezone
+       elseif(isset($attribute_array[1]) && in_array($attribute_array[0], $this->_dates))
+       {
+         $this->_logToSymfony('Timezone is: '.$attribute_array[1]);
+         $this->_timezone = $attribute_array[1];
+         $this->setAttribute($attribute_array[0], $value);
+       }
+     }
+    }
+   
+   /**
+    * _buildICalendar
+    * Builds a string in the iCalendar format for output purposes.
+    * Called by toString('icalendar') for printing object event.
+    * Timezones are currently assumed to be the same across all date
+    * attributes for events.
+    *
+    * @return void
+    * @author Josh Reynolds
+    */
+   
+   protected function _buildICalendar()
+    {
+      $out = "BEGIN:VEVENT\n";
+      foreach($this->_attributes as $key => $value)
+      {
+        //if key is a date, append the timezone
+        $keyout = (in_array($key, $this->_dates)) ? $key.';'.$this->_timezone : $key;
+        //if value is a string, just add it
+        if(is_string($value) && $value != '')
+        {
+          $out .= strtoupper($keyout).":".strtoupper($value)."\n";
+        }
+        //for now if value is an array, ignore it
+        elseif(is_array($value))
+        {
+          //do something about arrays here...
+        }
+      }
+      $out .= "END:EVENT\n";
+      return $out;
+    }
+   
+   
    protected function _sameType($first, $second)
    {
      if(is_string($first) && is_string($second))
@@ -258,5 +334,13 @@ class BasecsOopCalendarEvent
        return false;
      }
    }
+   
+   public function _logToSymfony($message = '')
+    {
+       if(isset($this->_logger))
+       {
+         $this->_logger->info('csOopCalendarEvent: '.$message);
+       }
+    }
 }
 ?>
